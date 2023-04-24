6656e53c0f153adb9dd04c2e5d413693ef3e1bd3
shakyaprasen <kajiprasen@gmail.com>
2023-04-24 09:16:29 +0545

    Enable vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,32 +25,34 @@ const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
-  // root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // outDir: './dist',       // When running `astro build`, path to final static output
-  // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  output: 'server',
-  site: 'https://astro-ink.vercel.app',
-  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  server: {
-    port: 3000,         // The port to run the dev server on.
-  },
-  integrations: [mdx(),
+    // root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+    // outDir: './dist',       // When running `astro build`, path to final static output
+    // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+    output: 'server',
+    site: 'https://astro-ink.vercel.app',
+    // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    server: {
+        port: 3000,         // The port to run the dev server on.
+    },
+    integrations: [mdx(),
     // markdoc(), // disabled now due to an issue with Vercel builds
     svelte(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap()],
-  vite: {
-    plugins: [],
-    resolve: {
-      alias: {
-        '$': path.resolve(__dirname, './src')
-      }
+        config: {
+            applyBaseStyles: false
+        }
+    }), sitemap()],
+    vite: {
+        plugins: [],
+        resolve: {
+            alias: {
+                '$': path.resolve(__dirname, './src')
+            }
+        },
+        optimizeDeps: {
+            allowNodeBuiltins: true
+        }
     },
-    optimizeDeps: {
-      allowNodeBuiltins: true
-    }
-  },
-  adapter: vercel()
+    adapter: vercel({
+        analytics: true
+    })
 });

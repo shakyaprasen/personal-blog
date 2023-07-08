f220b3f849153454be49ee45bf10edba332ce3cc
shakyaprasen <kajiprasen@gmail.com>
2023-07-08 20:56:45 +0545

    broken-interviews: add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import markdoc from "@astrojs/markdoc";
+import robotsTxt from "astro-robots-txt";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -25,34 +26,35 @@ const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
-    // root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-    // outDir: './dist',       // When running `astro build`, path to final static output
-    // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-    output: 'server',
-    site: 'https://astro-ink.vercel.app',
-    // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    server: {
-        port: 3000,         // The port to run the dev server on.
+  // root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  // outDir: './dist',       // When running `astro build`, path to final static output
+  // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  output: 'server',
+  site: 'https://www.prasenshakya.com.np/',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  server: {
+    port: 3000 // The port to run the dev server on.
+  },
+
+  integrations: [mdx(),
+  // markdoc(), // disabled now due to an issue with Vercel builds
+  svelte(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), robotsTxt()],
+  vite: {
+    plugins: [],
+    resolve: {
+      alias: {
+        '$': path.resolve(__dirname, './src')
+      }
     },
-    integrations: [mdx(),
-    // markdoc(), // disabled now due to an issue with Vercel builds
-    svelte(), tailwind({
-        config: {
-            applyBaseStyles: false
-        }
-    }), sitemap()],
-    vite: {
-        plugins: [],
-        resolve: {
-            alias: {
-                '$': path.resolve(__dirname, './src')
-            }
-        },
-        optimizeDeps: {
-            allowNodeBuiltins: true
-        }
-    },
-    adapter: vercel({
-        analytics: true
-    })
+    optimizeDeps: {
+      allowNodeBuiltins: true
+    }
+  },
+  adapter: vercel({
+    analytics: true
+  })
 });
